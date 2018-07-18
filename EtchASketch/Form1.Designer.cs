bd5a305e970a6bc58b5c5cf10436e53958f4aa04
Charlie Hoffmann <34170461+CharlieHoffmann@users.noreply.github.com>
2018-07-18 13:55:31 -0500

    Initial commit

---
+++ EtchASketch/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace EtchASketch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(28, 372);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(182, 51);
+            this.clearBtn.TabIndex = 0;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.Location = new System.Drawing.Point(507, 372);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(178, 51);
+            this.closeBtn.TabIndex = 1;
+            this.closeBtn.Text = "Close";
+            this.closeBtn.UseVisualStyleBackColor = true;
+            this.closeBtn.Click += new System.EventHandler(this.closeBtn_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(686, 324);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 458);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Name = "Form1";
+            this.Text = "Etch-A-Sketch";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ EtchASketch/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EtchASketch
+{
+    public partial class Form1 : Form
+    {
+        Bitmap DrawArea;
+        float penLoc1 = 10;
+        float penLoc2 = 10;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Red; // sets form color as red
+            DrawArea = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);//creates a bitmap the size of the picturebox
+            pictureBox1.Image = DrawArea;
+            pictureBox1.BackColor = Color.Gray; //sets picturebox color as gray
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+            Pen pen = new Pen(Brushes.Black);
+            g.DrawLine(pen, 0, 0, 200, 200);
+            
+           
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+  
+            //capture up arrow key
+             if (keyData == Keys.Up)
+            {
+                Graphics g;
+                g = Graphics.FromImage(DrawArea);
+
+                Pen mypen = new Pen(Color.Black);
+                if(penLoc2 !=0)
+                {
+                    g.DrawLine(mypen, penLoc1, penLoc2, penLoc1, penLoc2 - 10);
+                    penLoc2 = penLoc2 - 10;
+                }
+                pictureBox1.Image = DrawArea;
+
+                g.Dispose();
+               
+                
+               
+            
+            }
+            //capture down arrow key
+            else if (keyData == Keys.Down)
+            {
+                Graphics g;
+                g = Graphics.FromImage(DrawArea);
+
+                Pen mypen = new Pen(Color.Black);
+
+                if (penLoc2 != 320)
+                {
+                    g.DrawLine(mypen, penLoc1, penLoc2, penLoc1, penLoc2 + 10);
+                    penLoc2 = penLoc2 + 10;
+                }
+                pictureBox1.Image = DrawArea;
+
+                g.Dispose();
+
+                
+            }
+            //capture left arrow key
+            else if (keyData == Keys.Left)
+            {
+                Graphics g;
+                g = Graphics.FromImage(DrawArea);
+
+                Pen mypen = new Pen(Color.Black);
+
+                if (penLoc1 != 0)
+                {
+                    g.DrawLine(mypen, penLoc1, penLoc2, penLoc1 - 10, penLoc2);
+                    penLoc1 = penLoc1 - 10;
+                }
+                pictureBox1.Image = DrawArea;
+
+                g.Dispose();
+
+                
+            }
+            //capture right arrow key
+            else if (keyData == Keys.Right)
+            {
+                Graphics g;
+                g = Graphics.FromImage(DrawArea);
+
+                Pen mypen = new Pen(Color.Black);
+
+                if (penLoc1 != 680)
+                {
+                    g.DrawLine(mypen, penLoc1, penLoc2, penLoc1 + 10, penLoc2);
+                    penLoc1 = penLoc1 + 10;
+                    
+                }
+
+                pictureBox1.Image = DrawArea;
+
+                g.Dispose();
+
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            Graphics g;
+            g = Graphics.FromImage(DrawArea);
+
+            g.Clear(Color.Gray);
+
+            pictureBox1.Image = DrawArea;
+        }
+
+        private void closeBtn_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    } 
+}
+ 
